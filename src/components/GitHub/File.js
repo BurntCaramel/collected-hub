f5661fc6f9fcf7e0643249ee9a41fad0c384be04
Patrick Smith <patrick@burntcaramel.com>
2017-11-08 23:26:20 +1100

    Improve humanizing of camel case with multiple caps
    Before: APIErrorMessage -> APIError Message
    After: APIErrorMessage -> API Error Message

--- src/components/GitHub/File.js
+++ src/components/GitHub/File.js
@@ -2,7 +2,9 @@ import {h} from 'preact'
 
 const dropExtension = (name) => name.split('.').slice(0, -1).join('.')
 
-const humanize = (camelCase) => camelCase.replace(/([a-z])([A-Z])/g, '$1 $2')
+const humanize = (camelCase) => camelCase
+  .replace(/([a-z])([A-Z])/g, '$1 $2')
+  .replace(/([A-Z]+)([A-Z][a-z])/g, '$1 $2')
 
 export default ({
   name,
