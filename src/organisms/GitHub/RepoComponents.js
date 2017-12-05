80eacd757b5b351451a143cc4809ddfb7ee451c8
Patrick Smith <patrick@burntcaramel.com>
2017-12-05 17:54:31 +1100

    Change layout to sidebar

--- src/organisms/GitHub/RepoComponents.js
+++ src/organisms/GitHub/RepoComponents.js
@@ -7,28 +7,51 @@ export default ({
   jsComponents,
   activePath,
   onChangeActivePath
-}) => <div>
-  { jsComponents ? (
-    jsComponents.map((item) => (
-      <button
-        className={ classes([
-          'w-full text-left p-2',
-          item.path === activePath && 'active'
-        ]) }
-        onClick={ () => onChangeActivePath(item.path) }
-      >
-        <GitHubFile
-          key={ item.path }
-          { ...item }
-        />
-      </button>
-    ))
-  ) : (
-    'Loading…'
-  ) }
-  { !!activePath &&
-    <GitHubFileContent
-      url={ jsComponents.find(a => a.path === activePath).url }
-    />
-  }
+}) => <div className='relative'>
+  <div
+    className={
+      classes([
+        'hidden',
+        'md:flex',
+        'flex-col',
+        'md:max-w-xs',
+        'absolute',
+        'pin-y',
+        'pin-l',
+        'overflow-y-scroll',
+      ])
+    }
+  >
+    { jsComponents ? (
+      jsComponents.map((item) => (
+        <button
+          className={ classes([
+            'w-full text-left p-2',
+            item.path === activePath && 'active'
+          ]) }
+          onClick={ () => onChangeActivePath(item.path) }
+        >
+          <GitHubFile
+            key={ item.path }
+            { ...item }
+          />
+        </button>
+      ))
+    ) : (
+      'Loading…'
+    ) }
+  </div>
+  <div
+    className={
+      classes([
+        'md:ml-max-w-xs',
+      ])
+    }
+  >
+    { !!activePath &&
+      <GitHubFileContent
+        url={ jsComponents.find(a => a.path === activePath).url }
+      />
+    }
+  </div>
 </div>
